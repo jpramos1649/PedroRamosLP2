968d10128acc2f8bec56a9bcf748dee6d754b0a6
jpramos1649 <pedrinho1649@gmail.com>
2017-05-31 16:04:06 -0300

    ObjetoNoPlano

---
+++ ObjetosNoPlano/ObjetosNoPlano/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetosNoPlano
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Obejeto2D obj = new Obejeto2D();
+
+            Console.WriteLine("digite x inicial: ");
+            obj.x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite y inicial: ");
+            obj.y = int.Parse(Console.ReadLine());
+
+            
+            while(true)
+            {
+                
+             Console.WriteLine("digite o comando: ");
+             ConsoleKey comando = Console.ReadKey().Key;
+
+             if (comando == ConsoleKey.RightArrow)
+             {
+                 obj.AndarParaDireita();
+                 
+             }
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
